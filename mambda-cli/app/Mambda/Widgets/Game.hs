6cc08bcdf43d58db0fdb95e55d70b3761fbfa928
Tomasz Batko <batkot@protonmail.com>
2026-08-10 14:58:01 +0200

    Some small refactor

--- mambda-cli/app/Mambda/Widgets/Game.hs
+++ mambda-cli/app/Mambda/Widgets/Game.hs
@@ -10,8 +10,6 @@ import Prelude
 
 import Data.Functor.Identity
 
-import Aztecs qualified
-
 import Data.Map (Map)
 import Data.Map qualified as Map
 
@@ -26,39 +24,41 @@ import Linear
 
 newtype State = State (Game.State Identity)
 
-data Glyph = SnakeHead | Empty
-
-renderGlyph :: Glyph -> Brick.Widget n
-renderGlyph Empty = Brick.withAttr emptyAttr $ Brick.str "██"
-renderGlyph SnakeHead = Brick.withAttr snakeAttr $ Brick.str "██"
+renderGlyph :: Game.Glyph -> Brick.Widget n
+renderGlyph Game.Empty = Brick.withAttr emptyAttr $ Brick.str "██"
+renderGlyph Game.Snake = Brick.withAttr snakeAttr $ Brick.str "██"
+renderGlyph Game.Wall = Brick.withAttr snakeAttr $ Brick.str "██"
 
 initState :: State
-initState = State $ runIdentity Game.initWorld
+initState = State $ runIdentity Game.init
 
 handleEvent :: Brick.BrickEvent n e -> Brick.EventM n State ()
 handleEvent (Brick.AppEvent _) = Brick.modify $ \(State g) -> State $ runIdentity $ Game.step g
 handleEvent _ = pure ()
 
 render :: State -> Brick.Widget n
-render (State (Game.State world)) =
+render (State s) =
     Brick.center $
         Brick.vCenter $
-            Brick.borderWithLabel (Brick.str "Game") $
-                renderGrid frameGrid
+            Brick.borderWithLabel (Brick.str "Game") frame
   where
-    emptyGrid :: Map Integer (Map Integer Glyph)
-    emptyGrid = Map.fromList $ do
-        x <- [1 .. 10]
-        pure $ (x, Map.fromList $ [(y, Empty) | y <- [1 .. 10]])
-    renderGrid :: Map Integer (Map Integer Glyph) -> Brick.Widget n
-    renderGrid m =
-        let rows = Map.elems m
-            renderCol map' = Brick.hBox $ renderGlyph <$> Map.elems map'
-         in Brick.vBox $ renderCol <$> rows
-    foo = fst $ runIdentity $ Aztecs.runAccess Game.foo world
-    updateElement :: Glyph -> Integer -> Integer -> Map Integer (Map Integer Glyph) -> Map Integer (Map Integer Glyph)
-    updateElement glyph x y = Map.update (Just . Map.insert y glyph) x
-    frameGrid = Vector.foldl' (\m (Game.Position (V2 x y)) -> updateElement SnakeHead x y m) emptyGrid foo
+    Game.Render r = runIdentity $ Game.render s
+    frame = Brick.vBox $ Vector.toList $ fmap renderRow r
+    renderRow v = Brick.hBox $ Vector.toList $ renderGlyph <$> v
+
+-- emptyGrid :: Map Integer (Map Integer Glyph)
+-- emptyGrid = Map.fromList $ do
+--     x <- [1 .. 10]
+--     pure $ (x, Map.fromList $ [(y, Empty) | y <- [1 .. 10]])
+-- renderGrid :: Map Integer (Map Integer Glyph) -> Brick.Widget n
+-- renderGrid m =
+--     let rows = Map.elems m
+--         renderCol map' = Brick.hBox $ renderGlyph <$> Map.elems map'
+--      in Brick.vBox $ renderCol <$> rows
+-- foo = runIdentity $ Game.render s
+-- updateElement :: Glyph -> Integer -> Integer -> Map Integer (Map Integer Glyph) -> Map Integer (Map Integer Glyph)
+-- updateElement glyph x y = Map.update (Just . Map.insert y glyph) x
+-- frameGrid = Vector.foldl' (\m (Game.Position (V2 x y)) -> updateElement SnakeHead x y m) emptyGrid foo
 
 attributeMap :: Brick.AttrMap
 attributeMap =
--- mambda-cli/src/Mambda/Game.hs
+++ mambda-cli/src/Mambda/Game.hs
@@ -1,6 +1,13 @@
-module Mambda.Game where
+module Mambda.Game (
+    State,
+    init,
+    step,
+    render,
+    Render (..),
+    Glyph (..),
+) where
 
-import Prelude
+import Prelude hiding (init)
 
 import Aztecs qualified
 import Aztecs.ECS.World qualified as World
@@ -10,7 +17,15 @@ import Linear
 
 newtype State m = State {world :: Aztecs.World m}
 
+newtype Render = Render (Vector.Vector (Vector.Vector Glyph))
+
+data Glyph = Snake | Empty | Wall
+
 type Space = V2 Integer
+
+newtype World = World Space
+    deriving stock (Show)
+    deriving anyclass (Aztecs.Component m)
 
 newtype Position = Position Space
     deriving stock (Show)
@@ -27,23 +42,38 @@ newtype Lifetime = Lifetime Ticks
     deriving anyclass (Aztecs.Component m)
 
 snakeHead :: (Monad m) => Aztecs.BundleT m
-snakeHead = Aztecs.bundle (Position (V2 5 5)) <> Aztecs.bundle (Velocity (V2 1 0))
+snakeHead = Aztecs.bundle (Position (V2 1 1)) <> Aztecs.bundle (Velocity (V2 1 0))
 
 sampleWorld :: (Monad m) => Aztecs.Access m ()
-sampleWorld =
+sampleWorld = do
     void $ Aztecs.spawn snakeHead
+    void $ Aztecs.spawn $ Aztecs.bundle (World (V2 20 20))
 
-initWorld :: (Monad m) => m (State m)
-initWorld = State . snd <$> Aztecs.runAccess sampleWorld World.empty
+init :: (Monad m) => m (State m)
+init = State . snd <$> Aztecs.runAccess sampleWorld World.empty
 
 gameStep :: (Monad m) => Aztecs.Access m ()
 gameStep =
     void $ Aztecs.system $ Aztecs.runQuery $ Aztecs.queryMapWith move Aztecs.query
   where
-    move (Velocity v) (Position pos) = Position $ pos + v
+    move (Velocity v) (Position pos) = Position $ min (pos + v) (V2 19 19)
 
 step :: (Monad m) => State m -> m (State m)
 step (State world) = State . snd <$> Aztecs.runAccess gameStep world
 
-foo :: forall m. (Monad m) => Aztecs.Access m (Vector.Vector Position)
-foo = Aztecs.system $ Aztecs.runQuery $ Aztecs.query @m @Position
+render :: forall m. (Monad m) => State m -> m Render
+render (State world) = fst <$> Aztecs.runAccess doRender world
+
+doRender :: forall m. (Monad m) => Aztecs.Access m Render
+doRender = do
+    World (V2 width height) <- fmap Vector.last $ Aztecs.system $ Aztecs.runQuery $ Aztecs.query @m @World
+    positions <- Aztecs.system $ Aztecs.runQuery $ Aztecs.query @m @Position
+    let emptyGrid = Vector.replicate (fromInteger width) $ Vector.replicate (fromInteger height) Empty
+        fullGrid = Vector.foldr (writeElem Snake) emptyGrid positions
+    pure $ Render fullGrid
+
+writeElem :: a -> Position -> Vector.Vector (Vector.Vector a) -> Vector.Vector (Vector.Vector a)
+writeElem a (Position (V2 x y)) grid =
+    let row = grid Vector.! fromInteger x
+        newRow = row Vector.// [(fromInteger y, a)]
+     in grid Vector.// [(fromInteger x, newRow)]
